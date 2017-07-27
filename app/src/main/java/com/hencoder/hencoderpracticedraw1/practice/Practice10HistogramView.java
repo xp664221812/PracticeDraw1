c7a5a005060e225e3d4003c827f347b60a024968
xp664221812 <realxp141414@163.com>
2017-07-27 14:17:19 +0800

    finish draw herat draw

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,27 +2,47 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice10HistogramView extends View {
+    private Paint paint;
+    private Path path;
 
     public Practice10HistogramView(Context context) {
         super(context);
+        init();
     }
 
     public Practice10HistogramView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Practice10HistogramView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
+
+    private void init() {
+        path = new Path();
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        //画坐标
+        paint.setColor(Color.WHITE);
+        paint.setStrokeWidth(2.5f);
+        canvas.drawLine(100,10,100,750,paint);
+        canvas.drawLine(100,750,1200,750,paint);
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,6 +2,9 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -23,6 +26,18 @@ public class Practice9DrawPathView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL);
+        Path path = new Path();
+        paint.setColor(Color.BLACK);
+
+        path.arcTo(400, 300, 600, 500, 135, 210, false);
+        path.arcTo(600, 300, 800, 500, -175, 210, false);
+
+        path.lineTo(600, 650);
+        path.close();
+        canvas.drawPath(path, paint);
+
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
     }
