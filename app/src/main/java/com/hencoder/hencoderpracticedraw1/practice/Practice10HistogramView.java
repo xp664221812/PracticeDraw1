ec58e605760aa8ebaa15d504ee5c8f307946027e
xp664221812 <realxp141414@163.com>
2017-07-28 08:38:44 +0800

    histogramview

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -42,7 +42,30 @@ public class Practice10HistogramView extends View {
         paint.setColor(Color.WHITE);
         paint.setStrokeWidth(2.5f);
         canvas.drawLine(100,10,100,750,paint);
-        canvas.drawLine(100,750,1200,750,paint);
+        canvas.drawLine(100,750,1300,750,paint);
+
+        //画矩形
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.GREEN);
+        canvas.drawRect(130,749,260,750,paint);
+        canvas.drawRect(290,725,420,750,paint);
+        canvas.drawRect(450,725,580,750,paint);
+        canvas.drawRect(610,500,740,750,paint);
+        canvas.drawRect(770,300,900,750,paint);
+        canvas.drawRect(930,200,1060,750,paint);
+        canvas.drawRect(1090,550,1220,750,paint);
+
+        //画text
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(35);
+        canvas.drawText("Froyo",150,785,paint);
+        canvas.drawText("GB",330,785,paint);
+        canvas.drawText("ICS",480,785,paint);
+        canvas.drawText("JB",650,785,paint);
+        canvas.drawText("KiKat",790,785,paint);
+        canvas.drawText("L",980,785,paint);
+        canvas.drawText("M",1140,785,paint);
+
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
